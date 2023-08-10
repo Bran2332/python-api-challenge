e728a74fe8c862da85d5cab473db7cc038ed9352
Bran2332 <brandonchacon1993@outlook.com>
2023-08-09 20:04:35 -0400

    update

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "33237b5faddea0e4a21001ece65db305"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "ae541f612c70430e9b04b360a46487eb"
+geoapify_key = ""
